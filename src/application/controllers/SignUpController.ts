edf2031cedff3aa6ffbc95786eed14c554c81729
juanpablow <juan.santana.macedo10@outlook.com>
2024-10-10 15:10:29 -0300

    feat: add `SignUpController`

---
+++ src/application/controllers/SignUpController.ts
@@ -0,0 +1,35 @@
+import { z, ZodError } from "zod";
+
+import { IController, IReq, IRes } from "../interfaces/IController";
+import { SignUpUseCase } from "../useCases/SignUpUseCase";
+
+const schema = z.object({
+  name: z.string().min(2),
+  email: z.string().email().min(1),
+  password: z.string().min(8),
+});
+
+export class SignUpController implements IController {
+  async handle({ body }: IReq): Promise<IRes> {
+    try {
+      const { name, email, password } = schema.parse(body);
+
+      const signUpUseCase = new SignUpUseCase();
+
+      await signUpUseCase.execute({ name, email, password });
+
+      return {
+        statusCode: 204,
+        body: null,
+      };
+    } catch (error) {
+      if (error instanceof ZodError) {
+        return {
+          statusCode: 400,
+          body: error.issues,
+        };
+      }
+      throw error;
+    }
+  }
+}
