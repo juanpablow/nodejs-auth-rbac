015f5d816769cc6f52ed0fd0d2fb1e5742c87c28
juanpablow <juan.santana.macedo10@outlook.com>
2024-10-15 15:00:46 -0300

    refactor: update server to use `routeAdapter`

--- src/server/index.ts
+++ src/server/index.ts
@@ -2,32 +2,13 @@ import fastify from "fastify";
 
 import { makeSignInController } from "../factories/makeSignInController";
 import { makeSignupController } from "../factories/makeSignUpController";
+import { routeAdapter } from "../server/adapters/routeAdapters";
 
 const app = fastify();
 
-app.post("/signup", async (req, res) => {
-  const signUpController = makeSignupController();
+app.post("/signup", routeAdapter(makeSignupController()));
 
-  if (req.body != null) {
-    const { statusCode, body } = await signUpController.handle({
-      body: req.body,
-    });
-
-    res.status(statusCode).send(body);
-  }
-});
-
-app.post("/signin", async (req, res) => {
-  const signInController = makeSignInController();
-
-  if (req.body != null) {
-    const { statusCode, body } = await signInController.handle({
-      body: req.body,
-    });
-
-    res.status(statusCode).send(body);
-  }
-});
+app.post("/signin", routeAdapter(makeSignInController()));
 
 app.listen({ port: 3001 }, () => {
   console.log("Server started at http://localhost:3001");
