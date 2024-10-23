286b06f60e36d7d05da55c3cc0f2bfc56079dca7
juanpablow <juan.santana.macedo10@outlook.com>
2024-10-23 13:54:31 -0300

    refactor: update `controller` handle to include `body`, `params` and `accountId` - ensure `req.body` defaults to empty object - cast `req.params` and include `accountId` from `metadata`

--- src/server/adapters/routeAdapters.ts
+++ src/server/adapters/routeAdapters.ts
@@ -3,12 +3,11 @@ import { IController } from "../../application/interfaces/IController";
 
 export function routeAdapter(controller: IController) {
   return async (req: FastifyRequest, reply: FastifyReply) => {
-    if (req.body != null) {
-      const { statusCode, body } = await controller.handle({
-        body: req.body,
-      });
-
-      reply.status(statusCode).send(body);
-    }
+    const { statusCode, body } = await controller.handle({
+      body: req.body ?? {},
+      params: req.params as Record<string, string> | undefined,
+      accountId: req.metadata.accountId,
+    });
+    reply.status(statusCode).send(body);
   };
 }
