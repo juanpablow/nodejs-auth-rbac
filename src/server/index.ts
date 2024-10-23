d2cff38121bc9914714fe841cc5c7136b73ff278
juanpablow <juan.santana.macedo10@outlook.com>
2024-10-23 13:56:35 -0300

    feat: add `authentication middleware` and `route adapter` for list `/leads` endpoint - use `middlewareAdapter` with `makeAuthenticationMiddleware` - apply `routeAdapter` with `makeListLeadsController`

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,14 +1,24 @@
 import fastify from "fastify";
 
+import { makeAuthenticationMiddleware } from "../factories/makeAuthenticationMiddleware";
+import { makeListLeadsController } from "../factories/makeListLeadsController";
 import { makeSignInController } from "../factories/makeSignInController";
 import { makeSignupController } from "../factories/makeSignUpController";
 import { routeAdapter } from "../server/adapters/routeAdapters";
+import { middlewareAdapter } from "./adapters/middlewareAdapter";
 
 const app = fastify();
 
 app.post("/signup", routeAdapter(makeSignupController()));
-
 app.post("/signin", routeAdapter(makeSignInController()));
+
+app.get(
+  "/leads",
+  {
+    preHandler: middlewareAdapter(makeAuthenticationMiddleware()),
+  },
+  routeAdapter(makeListLeadsController())
+);
 
 app.listen({ port: 3001 }, () => {
   console.log("Server started at http://localhost:3001");
